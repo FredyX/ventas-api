1c1ab4c1464ae3973ae6f6a128b1f81b358e52ff
darkbehm <bbeehhmm@protonmail.com>
2022-03-06 18:16:06 -0600

    create profile_pictures module

---
+++ models/profile_pictures.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, type) => {
+    return sequelize.define('profile_pictures',{
+        id:{
+            type: type.INTEGER,
+            primaryKey:true,
+            autoIncrement: true
+        },
+        pp_name:{
+            type: type.STRING(10),
+            allowNull:false
+        },pp_data:{
+            type: type.mediumBlob
+        }
+    },
+    {
+        timestamps: false
+    });
+}
