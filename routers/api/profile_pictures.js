63a153f6e80bc450a4732f39a8f4354147f6e1a0
darkbehm <bbeehhmm@protonmail.com>
2022-03-06 19:27:49 -0600

    Update profile_pictures.js

--- routers/api/profile_pictures.js
+++ routers/api/profile_pictures.js
@@ -2,13 +2,16 @@ const router = require('express').Router();
 const { Profile_pictures } = require('../../config/db.config');
 
 router.get('/:id', async (req, res) => {
-
-    const profile_picture = await Profile_pictures.findOne({
-        where: { id: req.params.id }
-    })
-    res.writeHead(200, { 'Content-Type': 'image/png' });
-    res.end(profile_picture.pp_data, 'binary');
-
+    if (req.params.id < 17 && req.params.id > 0) {
+        const profile_picture = await Profile_pictures.findOne({
+            where: { id: req.params.id }
+        })
+        res.writeHead(200, { 'Content-Type': 'image/png' });
+        res.end(profile_picture.pp_data, 'binary');
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not found');
+    }
 });
 
 module.exports = router;
