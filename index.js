9b32e10ede78f316ffcba71cb352775bdff5ba14
darkbehm <bbeehhmm@protonmail.com>
2022-03-08 16:30:18 -0600

    update index.js to allow connections from all sources

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ const app = express();
 require('./config/db.config');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/api',apiRouter);
-app.listen( 3000, () => {
+app.listen( 3001, () => {
     console.log('servidor arranco correctamente');
 });
