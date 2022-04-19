f2ee0300c951a30f213cb83d1f3d26c5d2589361
JonHdez <Jonathan.DHernandezR@gmail.com>
2022-04-19 09:14:09 -0600

    add,delete, update categories
    Categories

--- routers/api/categories.js
+++ routers/api/categories.js
@@ -1,9 +1,45 @@
-const {Router} = require('express');
-const {getAllCategories,getCategoriesId} = require('../../controllers/categories');
-const router = Router();
+
+const router = require('express').Router();
+const { check } = require('express-validator');
+const {doCategoryIdExist} = require('../../helpers/dbValidators');
+const { validateFields, validateJwt, validateAdmin } = require('../../middlewares/');
+const {
+    getAllCategories,
+    getCategoriesId,
+    categoriesPutUpdate,
+    categoriesDelete,
+    categoriesPostAdd
+      } = require('../../controllers/categories');
 
 router.get('/', getAllCategories);
 router.get('/:id', getCategoriesId);
+
+router.delete('/:id', [
+    validateJwt,
+    check('id').not().isEmpty().withMessage('El id es obligatorio'),
+    doCategoryIdExist,
+    validateFields,
+    validateAdmin,
+], categoriesDelete);
+
+router.put('/:id', [
+    validateJwt,    
+    check('id').not().isEmpty().withMessage('El id es obligatorio'),
+    check('categorie_name').not().isEmpty().withMessage('El nombre de la categoria es obligatorio'),
+    check('id').not().isEmpty().withMessage('El id de la categoria es obligatorio'),
+    check('id').custom(doCategoryIdExist),
+    validateFields,
+    validateAdmin,
+], categoriesPutUpdate);
+
+router.post('/', [
+    validateJwt,
+    check('categorie_name').not().isEmpty().withMessage('El nombre de la categoria es obligatorio'),
+    check('id').not().isEmpty().withMessage('El id de la categoria es obligatorio'),
+    check('id').custom(doCategoryIdExist),
+    validateAdmin,
+    validateFields
+], categoriesPostAdd);
 
 module.exports = router;
 
